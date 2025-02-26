9233bc7080255aff2d47c76bc84396a0d0735239
Nakul Gopakumar <67837541+nakulgopakumar@users.noreply.github.com>
2025-02-26 17:02:14 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Nakul`;
+export const OWNER_DESCRIPTION: string = `is the student`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
