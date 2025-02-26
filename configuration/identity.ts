ab443fbd3719fda6701b78e29825c4652c2fa389
Nakul Gopakumar <67837541+nakulgopakumar@users.noreply.github.com>
2025-02-26 17:13:31 -0500

    Updated name and description of the assistant

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `Nakul`;
-export const OWNER_DESCRIPTION: string = `is the student`;
+export const OWNER_NAME: string = `Baseline Ventures`;
+export const OWNER_DESCRIPTION: string = `Founded in 2006 by Steve Anderson, Baseline Ventures focuses on seed and growth-stage investments in technology companies. Notably, it was the first seed investor in Instagram and an early investor in Twitter`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Dealora`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
